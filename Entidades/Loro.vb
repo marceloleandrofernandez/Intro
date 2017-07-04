59871c70c2104b85e49754c1a7ce7c7c477bd719
marceloleandrofernandez <marceloleandrofernandez@gmail.com>
2017-07-04 01:05:58 -0300

    4/7

---
+++ Entidades/Loro.vb
@@ -0,0 +1,46 @@
+﻿Public Class Loro
+    Private _memoria As Queue(Of String)
+    Private _fechaNacimiento As Date
+    Private _nombre As String
+    Sub New()
+        _memoria = New Queue(Of String)
+        _fechaNacimiento = #9/21/1985#
+        _nombre = "Pepe"
+    End Sub
+    Public Property FechaNacimiento As Date
+        Get
+            Return _fechaNacimiento
+        End Get
+        Set(value As Date)
+            _fechaNacimiento = value
+        End Set
+    End Property
+    Public Property Nombre As String
+        Get
+            Return _nombre
+        End Get
+        Set(value As String)
+            _nombre = value
+        End Set
+    End Property
+    Public ReadOnly Property Edad As Short
+        Get
+            Return CalcularEdad()
+        End Get
+    End Property
+    Private Function CalcularEdad() As Short
+        Return DateDiff(DateInterval.Year, FechaNacimiento, DateTime.Now)
+    End Function
+    Public Sub Escuchar(frase As String)
+        _memoria.Enqueue(frase)
+    End Sub
+    Public Overrides Function ToString() As String
+        Return Nombre
+    End Function
+    Public Function Hablar() As String
+        If _memoria.Count > 1 Then
+            Return _memoria.Dequeue
+        End If
+        Return ""
+    End Function
+End Class
---
+++ EntidadesTest/LoroTest.vb
@@ -0,0 +1,29 @@
+﻿Imports Entidades
+Module LoroTest
+
+    Sub Main()
+        Dim lodo As New Loro
+        Dim cadena As String = ""
+        lodo.Nombre = "Ramon"
+        lodo.FechaNacimiento = #12/12/1990#
+        Console.WriteLine("Nombre = {0}", lodo.Nombre)
+        Console.WriteLine("Edad = {0}", lodo.Edad)
+
+
+        lodo.Escuchar("Papita")
+        lodo.Escuchar("pa el loro")
+        lodo.Escuchar("Juan")
+
+        cadena += lodo.Hablar()
+        Console.WriteLine(cadena)
+        cadena += lodo.Hablar()
+        Console.WriteLine(cadena)
+        cadena += lodo.Hablar()
+        Console.WriteLine(cadena)
+        cadena += lodo.Hablar()
+        Console.WriteLine(cadena)
+
+        Console.ReadKey()
+    End Sub
+
+End Module
