bdb57d1f7ab2e8002e954d532936dbbd2bf81668
marceloleandrofernandez <marceloleandrofernandez@gmail.com>
2018-04-10 10:16:13 -0300

    Add files via upload
    TP 1

---
+++ SEMINARIO/Unidad 1/EjerciciciosConsulta.sql
@@ -0,0 +1,47 @@
+
+-- Ejercicio 1.1 --
+
+SELECT *
+FROM cargos
+
+-- Ejercicio 1.2 --
+
+SELECT empleados.*,cargo_descripcion
+FROM empleados,cargos
+
+-- Ejercicio 1.3 --
+--PREGUNTA algo mas facil para renombrar?--
+
+SELECT 
+empleado_id as 'empleado id',inicial_segundo_nombre as 'inicial segundo nombre' ,
+cargo_id as 'cargo id',nivel_cargo as 'nivel cargo',editorial_id as 'editorial id',
+fecha_contratacion as 'fecha contratacion'
+FROM empleados
+
+-- Ejercicio 1.4 --
+
+SELECT getdate() as 'Fecha y hora actual'
+
+-- Ejercicio 1.5 --
+--Preguntar lo de que multipla las lineas.--
+SELECT titulo, 'Regalías x cada 1000 unidades'=regalias * 1000 
+FROM titulos
+
+-- Ejercicio 1.6 --
+SELECT DISTINCT autor_nombre
+FROM autores
+
+-- Ejercicio 1.7 --
+SELECT DISTINCT pais
+FROM editoriales
+
+-- Ejercicio 1.8 --
+
+SELECT DISTINCT almacen_nombre
+FROM almacenes
+
+
+-- Ejercicio 1.9 --
+
+SELECT TOP 5 almacen_nombre,cantidad
+FROM almacenes, ventas
